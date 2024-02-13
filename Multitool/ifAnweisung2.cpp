206b5ac515325a34c75fbbc6d9420a7e0ca96d7f
Riano-Francesco <riano.forni@web.de>
2024-02-13 12:38:55 +0100

    Push

--- Multitool/ifAnweisung2.cpp
+++ Multitool/ifAnweisung2.cpp
@@ -10,11 +10,11 @@ int ifA2(int a, int b) {
 	int erg = a + b;
 
 	if (erg < 20) {
-		cout << "Ergebnis ist kleiner als 20\n\n\n";
+		cout << erg << " ist kleiner als 20\n\n\n";
 	}
 	else {
-		cout << "Ergebnis ist größer als 20\n\n\n";
+		cout << erg << " ist größer als 20\n\n\n";
 	}
 
-	return (a + b);
+	return (erg);
 }
