f57d00d312407a72a261eaafec8bab1815f690b8
Riano-Francesco <riano.forni@web.de>
2024-02-16 12:48:14 +0100

    Push

--- Multitool/Continue.cpp
+++ Multitool/Continue.cpp
@@ -19,12 +19,12 @@ void ContinueSchleife() {
 		for (x = 1; x <= 3; x++) {
 
 			for (y = 1; y <= 5; y++) {
-
-				if ((x == 2) && (y == 3)) {
+				
+				if ((x == 2) && (y == 3)) { // Diese Bedingung wird ausgesetzt, beendet einen Schleifendurchlauf
 
 					cout << "continue" << endl;
 					continue;					// springt in den Kopf der Schleife wo sie enthalten ist. Bedingung erfüllt und übersprungen. Schleife geht dann normal weiter
-				}
+				}								// break beendet Schleife komplett und geht dann bei 3 weiter
 				cout << x << " : " << y << endl;
 			}
 		}
