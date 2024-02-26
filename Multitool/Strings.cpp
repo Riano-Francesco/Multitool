a0eb98a3ff5a68727b9e808795be65b5ca3494cf
Riano-Francesco <riano.forni@web.de>
2024-02-26 15:12:01 +0100

    push

--- Multitool/Strings.cpp
+++ Multitool/Strings.cpp
@@ -23,8 +23,8 @@ using namespace std;
 //	return 0;
 //}
 
-// Upper Case: 65 - 90   Lower Case: 97 - 122
 
+//funktion um strings zu kopieren
 void ostrcpy(char* start, char* ziel) {
 	int i = 0;
 	while (ziel[i]) {
@@ -34,6 +34,7 @@ void ostrcpy(char* start, char* ziel) {
 	start[i] = ziel[i];
 }
 
+//funktion um die länge eines strings zu ermitteln
 int ostrlen(char* gr) {
 
 	int x = 0;
@@ -56,6 +57,8 @@ int ostrlen(char* gr) {
 //	return 0;
 //}
 
+//65-90 Uppercase | 97-122 Lowercase
+//funktion um einen string in uppercase zu konvertieren
 void upper(char* range) {
 	int count = 0;
 	for (int i = 0; i < ostrlen(range); i++) {
@@ -68,6 +71,18 @@ void upper(char* range) {
 	cout << range << endl << "Anzahl getauschter Buchstaben: " << count;
 }
 
+//funktion um einen string in lowercase zu konvertieren
+//void lowstring(char* str) {
+//	int i = 0;
+//	while (str[i]) {
+//		if (str[i] >= 65 && str[i] <= 90) {
+//			str[i] = str[i] + 32;
+//		}
+//		i++;
+//	}
+//}
+
+//funktion um einen string umzudrehen
 void reversestring(char* string) {
 	int a = 0;
 	int b = ostrlen(string) - 1;
@@ -80,6 +95,7 @@ void reversestring(char* string) {
 	}
 }
 
+//funktion um zwei strings zu mixen
 void mixstring(char* ziel, char* quelle1, char* quelle2) {
 	int a = 0;
 	int b = 0;
@@ -96,6 +112,7 @@ void mixstring(char* ziel, char* quelle1, char* quelle2) {
 	ziel[c] = '\0';
 }
 
+//funktion um zu prüfen ob ein string in einem anderen string vorkommt
 int ostrstr(char* pattern, char* check) {
 	int i = 0;
 	int j = 0;
@@ -116,6 +133,40 @@ int ostrstr(char* pattern, char* check) {
 		i++;
 	}
 	return -1;
+}
+
+//funktion um die ersten zeichen eines strings zu kopieren und in einen anderen string zu speichern
+void leftstr(char* ziel, char* quelle, int anz) {
+	int a = 0;
+
+	while (a < anz) {
+		ziel[a] = quelle[a];
+		a++;
+	}
+	ziel[a] = '\0';
+}
+
+//funktion um die letzten zeichen eines strings zu kopieren und in einen anderen string zu speichern
+void rightstr(char* ziel, char* quelle, int anz) {
+	int a = 0;
+	int b = ostrlen(quelle) - anz;
+	while (quelle[b]) {
+		ziel[a] = quelle[b];
+		a++;
+		b++;
+	}
+	ziel[a] = '\0';
+}
+
+//funktion um die Position festzulegen wo der String ausgegeben wird
+void midstr(char* ziel, char* quelle, int pos, int anz) {
+	int a = 0;
+	while (a < anz) {
+		ziel[a] = quelle[pos];
+		a++;
+		pos++;
+	}
+	ziel[a] = '\0';
 }
 
 void strings() {
@@ -141,9 +192,6 @@ void strings() {
 		char start[81];
 		char ziel[81];
 		char start2[81];
-
-		char check[81] = "Otto";
-		char pattern[81] = "Und Otto ging Baden";
 
 		cout << "Bitte geben Sie drei Wörter mit max. 80 Zeichen ein, wobei das zweite und dritte kombiniert wird: " << endl;
 		cin.getline(start, 80);
@@ -177,6 +225,10 @@ void strings() {
 		cout << "Ausgabe Mixed geschrieben: " << start;
 		cout << endl;
 
+
+		char check[81] = "Otto";
+		char pattern[81] = "Und Otto ging Baden";
+
 		int otto = ostrstr(check, pattern);
 		if (otto == -1) {
 			cout << "Das Wort \"" << check << "\" ist nicht in dem Satz \"" << pattern << "\" enthalten." << endl;
@@ -186,6 +238,28 @@ void strings() {
 			cout << "Das Wort \"" << check << "\" ist in dem Satz \"" << pattern << "\" enthalten." << endl;
 			cout << "Die Position ist: " << otto << endl;
 		}
+
+
+		char wortwahl[81]; char zielausgabe[81];
+		int anz = 0;
+		int position = 0;
+
+		cout << "Geben Sie ein Wort mit maximal 80 Zeichen ein: ";
+		cin >> wortwahl;
+
+		cout << "Wie viele Zeichen sollen im Ziel kopiert werden? -> ";
+		cin >> anz;
+
+		leftstr(zielausgabe, wortwahl, anz);
+		cout << "Stringzeichen von links aus ihren Eingaben: " << zielausgabe << endl;
+
+		rightstr(zielausgabe, wortwahl, anz);
+		cout << "Stringzeichen von rechts aus ihren Eingaben: " << zielausgabe << endl;
+
+		cout << "Bei welcher Stelle ihres Wortes wollen sie beginnen? -> ";
+		cin >> position;
+		midstr(zielausgabe, wortwahl, position, anz);
+		cout << "Stringzeichen von der gewählten Position aus ihren Eingaben: " << zielausgabe << endl;
 
         cout << endl;
         abbruch = wiederholung();
